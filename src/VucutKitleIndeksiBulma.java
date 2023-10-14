8a10796fdb1ea7c17f209aedfad146ec798a2572
Mustafa Haita <mustafahaitaa@gmail.com>
2023-10-14 19:13:10 +0300

    Vücut Kilte İndeksi Hesaplama

---
+++ src/VucutKitleIndeksiBulma.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksiBulma {
+    public static void main(String[] args) {
+        double kilo, boy, bmi;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz: ");
+        boy = input.nextDouble();
+
+        System.out.print("Lüttfen kilonuzu giriniz: ");
+        kilo = input.nextDouble();
+
+        bmi = kilo / Math.sqrt(boy);
+        System.out.println("Vücüt Kitle İndeksiniz: " + bmi);
+
+    }
+}
