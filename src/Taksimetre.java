6f877ce6d5033e60b0f8c1ec7bac6964fef5f192
Mustafa Haita <mustafahaitaa@gmail.com>
2023-10-14 19:00:53 +0300

    Taksimetre Programı

---
+++ src/Taksimetre.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Taksimetre {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double km, kmBasiTutar = 2.20, minimumTutar = 20, acilisUcreti = 10;
+
+        System.out.print("Gidilecek kilometre: ");
+        km = input.nextDouble();
+
+        double toplamTutar = (km * kmBasiTutar) + acilisUcreti;
+
+        double ucretLimiti = toplamTutar < minimumTutar ? toplamTutar = 20 : toplamTutar;
+
+        System.out.println("Toplam tutar: " + ucretLimiti);
+
+    }
+}
