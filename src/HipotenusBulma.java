7036bb67ebb6b17ae5e118aa28c850fb3497089b
Mustafa Haita <mustafahaitaa@gmail.com>
2023-10-14 18:55:03 +0300

    Dik Üçgende Hipotenüs Bulan Program

--- src/HipotenusBulma.java
+++ src/HipotenusBulma.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class HipotenusBulma {
     public static void main(String[] args) {
-    int a, b;
-    double c;
+    int a, b, c;
+    double u, alan;
     Scanner input = new Scanner(System.in);
 
     System.out.print("A sayısını giriniz: ");
@@ -12,8 +12,19 @@ public class HipotenusBulma {
     System.out.print("B sayısını giriniz: ");
     b = input.nextInt();
 
+    System.out.print("C sayısını giriniz: ");
+    c = input.nextInt();
+
+    u = (double) (a + b + c) / 2;
+    System.out.println("Üçgenin çevresi: " + (2 * u));
+
+    alan = u * (u - a) * (u - b) * (u - c);
+    System.out.println("Üçgenin alanı: " + alan);
+
+    /*
     c = Math.sqrt((a * a) + (b * b));
     System.out.println("Hipotenüs: " + c);
+    */
 
 
     }
