b98d0cf2fc10b890ab2e3562b4f6e2ca2e8064f6
Mustafa Haita <mustafahaitaa@gmail.com>
2023-10-14 19:22:49 +0300

    Manav Kasa Programı

---
+++ src/ManavKasa.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ManavKasa {
+    public static void main(String[] args) {
+        double armut = 2.14, elma = 3.67, domates = 1.11, muz = 0.95, patlican = 5, toplam = 0;
+        int kilo;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Armut kaç kilo? :");
+        kilo = input.nextInt();
+        toplam += armut * kilo;
+
+        System.out.print("Elma kaç kilo? :");
+        kilo = input.nextInt();
+        toplam += elma * kilo;
+
+        System.out.print("Domates kaç kilo? :");
+        kilo = input.nextInt();
+        toplam += domates * kilo;
+
+        System.out.print("Muz kaç kilo? :");
+        kilo = input.nextInt();
+        toplam += muz * kilo;
+
+        System.out.print("Patlıcan kaç kilo? :");
+        kilo = input.nextInt();
+        toplam += patlican * kilo;
+
+        System.out.println("Toplam tutar: " + toplam + "TL");
+    }
+}
