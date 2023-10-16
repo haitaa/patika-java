a01c5351b952cb3bd969cca9516ab5250c841e75
Mustafa Haita <mustafahaitaa@gmail.com>
2023-10-16 20:32:09 +0300

    Üslü Sayı Bulan Program

---
+++ src/SayininUssunuHesaplama.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SayininUssunuHesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayi, toplam=1, us;
+
+        System.out.print("Bir sayı giriniz: ");
+        sayi = input.nextInt();
+
+        System.out.print("Sayının üssünü giriniz: ");
+        us = input.nextInt();
+
+        for(int i = 1; i <= us; i++) {
+            toplam *= sayi;
+        }
+        System.out.println("Girilen sayının üssü: "+ toplam);
+    }
+}
