a89976fde69fc616aa8bf869d1786b39057ef1e0
Mustafa Haita <mustafahaitaa@gmail.com>
2023-10-16 19:49:39 +0300

    Girilen Sayıya Kadar Olan Çift Sayıları Bulan Program

---
+++ src/UceVeDordeBolunenSayilarinOrtalamasi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UceVeDordeBolunenSayilarinOrtalamasi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sayi, toplam = 0, dongu = 0;
+
+        System.out.print("Bir sayı giriniz: ");
+        sayi = input.nextInt();
+
+        for(int i = 0; i < sayi; i++) {
+            if(i % 3 == 0 || i % 4 == 0) {
+                System.out.println(i);
+                toplam += i;
+                dongu++;
+            }
+        }
+
+        int average = toplam / dongu;
+        System.out.println("Girilen sayıların ortalaması: " + average + ".");
+
+    }
+}
