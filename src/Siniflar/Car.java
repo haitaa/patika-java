984f3f0cf345dcfe6fbeaca23bf79a49eaf89f73
Mustafa Haita <mustafahaitaa@gmail.com>
2023-10-20 14:31:26 +0300

    Sınıf Tanımları

---
+++ src/Siniflar/Car.java
@@ -0,0 +1,23 @@
+package Siniflar;
+
+class Car {
+    String type;
+    String model;
+    String color;
+    int speed;
+    int speedLimit = 180;
+
+    void increaseSpeed(int increment) {
+        if((speed + increment) < speedLimit)
+            speed += increment;
+    }
+
+    void decreaseSpeed(int decrease) {
+        if(speed > 0)
+            speed -= decrease;
+    }
+
+    void printSpeed() {
+        System.out.println("Hızınız: " + speed);
+    }
+}
